6b5aa78f98fc197e76301fb47b51245fada7b8c1
JoseGarciaMoreno <64783493+JoseGarciaMoreno@users.noreply.github.com>
2020-12-26 21:24:12 +0100

    scrip menu principal

--- Main/Ejecutable/Menus/Manu Principal/MenuPrincipal.gd
+++ Main/Ejecutable/Menus/Manu Principal/MenuPrincipal.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
-#var preNivel_juegar_1 = preload("res://Ejecutable/Juego/Escenarios/Niveles/Nivel1/Nivel1.tscn")
+var preEscCarga3D = preload("res://Ejecutable/Menus/Cargar/Cargar3D.tscn")
+var EscCarga3D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+## Called when the node enters the scene tree for the first time.
+#func _ready():
+#	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,5 +14,10 @@ func _ready():
 
 
 func _on_TextureButton_pressed():
-	print("Hola")
+	cargar_pantallaCarga3D(preEscCarga3D)
 	pass # Replace with function body.
+
+func cargar_pantallaCarga3D(preEscCar):
+	EscCarga3D = preEscCar.instance()
+	get_node("/root").add_child(EscCarga3D)
+	queue_free()
