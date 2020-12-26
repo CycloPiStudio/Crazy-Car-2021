c59f968d81061218b21a4722918df20bf6dc6f90
CycloPi <venti3d@gmail.com>
2020-12-26 20:26:04 +0100

    Subo menu título y pantalla de carga

---
+++ Main/Ejecutable/Menus/Manu Principal/MenuPrincipal.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+#var preNivel_juegar_1 = preload("res://Ejecutable/Juego/Escenarios/Niveles/Nivel1/Nivel1.tscn")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_TextureButton_pressed():
+	print("Hola")
+	pass # Replace with function body.
